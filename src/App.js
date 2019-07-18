dcc108b506f00a509e393d5706191a874a40eefe
Justin Covell <jujugoboom@gmail.com>
2019-07-18 01:13:49 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import NavBar from "./components/Navbar";
+
+// New - import the React Router components, and the Profile page component
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
+
+function App() {
+  return (
+    <div className="App">
+      {/* New - use BrowserRouter to provide access to /profile */}
+      <BrowserRouter>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <Route path="/profile" component={Profile} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
